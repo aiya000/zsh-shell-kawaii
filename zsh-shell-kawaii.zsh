03da3533a945c29690c45dbd63d87e54ff03e79c
aiya000 <aiya000.develop@gmail.com>
2016-07-08 01:14:24 +0900

    Update zsh-shell-kawaii.zsh
    Kawaii Maid will thinking exit statuses

--- zsh-shell-kawaii.zsh
+++ zsh-shell-kawaii.zsh
@@ -3,14 +3,17 @@
 # Initialize confirations
 source $(dirname $0)/init.zsh
 
+# Define Kawaii Maid with exit statuses
+kawaii_maid="%(?.%{${fg_bold[green]}%}.%{${fg_bold[blue]}%})%(?!(*^-^)!(*~_~%))%{${reset_color}%}"
+
 # Set fake host name
 if [ $SHELL_KAWAII_HOST_VISIBILITY -eq 1 ] ; then
 	kawaii_info="%{$fg[yellow]%}${SHELL_KAWAII_FAKE_USERNAME}%{$fg[green]%}@%{$fg[yellow]%}${SHELL_KAWAII_FAKE_HOSTNAME}%{$reset_color%}"
 fi
 
-# Show kawaii character
+# Show Kawaii Maid
 if [ $SHELL_KAWAII_HER_VISIBILITY -eq 1 ] ; then
-	kawaii_face="%{$fg[green]%}(*^-^)</%{$fg[yellow]%}${kawaii_info}%{$fg[green]%}/%{$reset_color%}"
+	kawaii_face="${kawaii_maid}%{$fg[green]%}</%{$fg[yellow]%}${kawaii_info}%{$fg[green]%}/%{$reset_color%}"
 else
 	kawaii_face="${kawaii_info}"
 fi
