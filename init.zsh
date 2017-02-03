1d1a45e090189e341dc3299f404c1609e44578f8
aiya000 <aiya000.develop@gmail.com>
2017-02-04 00:40:35 +0900

    Add the comments

--- init.zsh
+++ init.zsh
@@ -1,21 +1,23 @@
 #!/usr/bin/env zsh
+# Define the variables with the default value
 
-# Set default values
+# Kawaii visit on the prompt
 if [ -z "$SHELL_KAWAII_HER_VISIBILITY" ] ; then
-	# Kawaii visit on the prompt
+	# default:0, valid:0 or 1
 	export SHELL_KAWAII_HER_VISIBILITY=0
 fi
 
 if [ -z "$SHELL_KAWAII_FAKE_USERNAME" ] ; then
-	# No Fake UserName
+	# default:$USER, valid:string
 	export SHELL_KAWAII_FAKE_USERNAME=$USER
 fi
 
 if [ -z "$SHELL_KAWAII_FAKE_HOSTNAME" ] ; then
-	# No Fake HostName
+	# default:$HOSTNAME, valid:string
 	export SHELL_KAWAII_FAKE_HOSTNAME=$HOSTNAME
 fi
 
 if [ -z "$SHELL_KAWAII_HOST_VISIBILITY" ] ; then
+	# default:1, valid:0 or 1
 	export SHELL_KAWAII_HOST_VISIBILITY=1
 fi
