871af48e7f51ae134f63a566bec521ee711072cb
aiya000 <aiya000.develop@gmail.com>
2017-02-04 00:50:11 +0900

    Fix global value polluting

--- zsh-shell-kawaii.zsh
+++ zsh-shell-kawaii.zsh
@@ -3,23 +3,27 @@
 # Initialize confirations
 source $(dirname $0)/init.zsh
 
-# Define Kawaii Maid with exit statuses
-kawaii_maid="%(?.%{${fg_bold[green]}%}.%{${fg_bold[blue]}%})%(?!(*^-^)!(;^-^%))%{${reset_color}%}"
+build_prompt () {
+	# Define Kawaii Maid with exit statuses
+	local maid="%(?.%{${fg_bold[green]}%}.%{${fg_bold[blue]}%})%(?!(*^-^)!(;^-^%))%{${reset_color}%}"
 
-# Set fake host name
-if [ $SHELL_KAWAII_HOST_VISIBILITY -eq 1 ] ; then
-	kawaii_info="%{$fg[yellow]%}${SHELL_KAWAII_FAKE_USERNAME}%{$fg[green]%}@%{$fg[yellow]%}${SHELL_KAWAII_FAKE_HOSTNAME}%{$reset_color%}"
-fi
+	# Set fake host name
+	if [ $SHELL_KAWAII_HOST_VISIBILITY -eq 1 ] ; then
+		local info="%{$fg[yellow]%}${SHELL_KAWAII_FAKE_USERNAME}%{$fg[green]%}@%{$fg[yellow]%}${SHELL_KAWAII_FAKE_HOSTNAME}%{$reset_color%}"
+	fi
 
-# Show Kawaii Maid
-if [ $SHELL_KAWAII_HER_VISIBILITY -eq 1 ] ; then
-	kawaii_face="${kawaii_maid}%{$fg[green]%}</%{$fg[yellow]%}${kawaii_info}%{$fg[green]%}/%{$reset_color%}"
-else
-	kawaii_face="${kawaii_info}"
-fi
+	# Show Kawaii Maid
+	if [ $SHELL_KAWAII_HER_VISIBILITY -eq 1 ] ; then
+		local face="${maid}%{$fg[green]%}</%{$fg[yellow]%}${info}%{$fg[green]%}/%{$reset_color%}"
+	else
+		local face="${info}"
+	fi
 
-# Show current directory
-kawaii_current_dir="%{$fg[yellow]%}%~%{$reset_color%}"
+	# Show current directory
+	local current_dir="%{$fg[yellow]%}%~%{$reset_color%}"
 
-# Apply Kawaii Maid
-PROMPT="${kawaii_face} ${kawaii_current_dir}%(!.#.$)%{$reset_color%} "
+	# Apply Kawaii Maid
+	export PROMPT="${face} ${current_dir}%(!.#.$)%{$reset_color%} "
+}
+
+build_prompt
