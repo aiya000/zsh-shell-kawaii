5770b9401ac3f3ecce8de0f436e67cc8742db7ed
aiya000 <aiya000.develop@gmail.com>
2017-02-04 02:25:24 +0900

    Refactor zsh-shell-kawaii.zsh

--- zsh-shell-kawaii.zsh
+++ zsh-shell-kawaii.zsh
@@ -5,31 +5,30 @@ source $(dirname $0)/init.zsh
 add-zsh-hook precmd build_prompt
 
 build_prompt () {
-	# Define Kawaii Maid with exit statuses
-	local maid="%(?.%{${fg_bold[green]}%}.%{${fg_bold[blue]}%})%(?!(*^-^)!(;^-^%))%{${reset_color}%}"
-
-	# Set fake host name
-	if [ $SHELL_KAWAII_HOST_VISIBILITY -eq 1 ] ; then
-		local info="%{$fg[yellow]%}${SHELL_KAWAII_FAKE_USERNAME}%{$fg[green]%}@%{$fg[yellow]%}${SHELL_KAWAII_FAKE_HOSTNAME}%{$reset_color%}"
+	# Define the variable. It looks like 'john@ArchLinux' or 'john'
+	local user_env="%{$fg[yellow]%}${SHELL_KAWAII_FAKE_USERNAME}%{$fg[green]%}"
+	if [ "$SHELL_KAWAII_HOST_VISIBILITY" -eq 1 ] ; then
+		local user_env="${user_env}@%{$fg[yellow]%}${SHELL_KAWAII_FAKE_HOSTNAME}%{$reset_color%}"
 	fi
 
-	# Show Kawaii Maid
-	if [ $SHELL_KAWAII_HER_VISIBILITY -eq 1 ] ; then
-		local face="${maid}%{$fg[green]%}</%{$fg[yellow]%}${info}%{$fg[green]%}/%{$reset_color%}"
+	# Define the expression of previous exit code
+	if [ "$SHELL_KAWAII_HER_VISIBILITY" -eq 1 ] ; then
+		# Define the maid. She expresses the state of previous exit code
+		local maid="%(?.%{${fg_bold[green]}%}.%{${fg_bold[blue]}%})%(?!(*^-^)!(;^-^%))%{${reset_color}%}"
+		local feature="${maid}%{$fg[green]%}</%{$fg[yellow]%}${user_env}%{$fg[green]%}/%{$reset_color%}"
 	else
-		local face="${info}"
+		local feature="${user_env}"
 	fi
 
-	# Show current directory
 	local current_dir="%{$fg[yellow]%}%~%{$reset_color%}"
 	local delim_pattern="%{$fg[cyan]%}%(!.#.${SHELL_KAWAII_USER_DELIMITER})%{$reset_color%}"
 
-	# Apply Kawaii Maid
+	# Apply result
 	if [ "$SHELL_KAWAII_CLI_TO_LOW" -eq 1 ] ; then
 		#FIXME: How can I set the linebreak without this notation ?
-		export PROMPT="${face} ${current_dir}%{$reset_color%}
+		export PROMPT="${feature} ${current_dir}%{$reset_color%}
 ${delim_pattern} "
 	else
-		export PROMPT="${face} ${current_dir}${delim_pattern}%{$reset_color%} "
+		export PROMPT="${feature} ${current_dir}${delim_pattern}%{$reset_color%} "
 	fi
 }
