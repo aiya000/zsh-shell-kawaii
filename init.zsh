9a9dd53d5bf70fee65763cbfb38b467ffb7e7d3a
aiya000 <aiya000.develop@gmail.com>
2019-06-15 22:52:55 +0900

    :recycle: Refactor

--- init.zsh
+++ init.zsh
@@ -1,41 +1,41 @@
 #!/usr/bin/env zsh
+
 # Define the variables with the default value
 
-# Kawaii visit on the prompt
-if [ -z "$SHELL_KAWAII_HER_VISIBILITY" ] ; then
-	# default:0, valid:0 or 1
-	export SHELL_KAWAII_HER_VISIBILITY=0
+if [[ -z $SHELL_KAWAII_HER_VISIBILITY ]] ; then
+    # default:0, valid:0 or 1
+    export SHELL_KAWAII_HER_VISIBILITY=0
 fi
 
-if [ -z "$SHELL_KAWAII_FAKE_USERNAME" ] ; then
-	# default:$(whoami), valid:string
-	export SHELL_KAWAII_FAKE_USERNAME=$(whoami)
+if [[ -z $SHELL_KAWAII_FAKE_USERNAME ]] ; then
+    # default:$(whoami), valid:string
+    export SHELL_KAWAII_FAKE_USERNAME=$(whoami)
 fi
 
-if [ -z "$SHELL_KAWAII_FAKE_HOSTNAME" ] ; then
-	# default:$(hostname), valid:string
-	export SHELL_KAWAII_FAKE_HOSTNAME=$(hostname)
+if [[ -z $SHELL_KAWAII_FAKE_HOSTNAME ]] ; then
+    # default:$(hostname), valid:string
+    export SHELL_KAWAII_FAKE_HOSTNAME=$(hostname)
 fi
 
-if [ -z "$SHELL_KAWAII_HOST_VISIBILITY" ] ; then
-	# default:1, valid:0 or 1
-	export SHELL_KAWAII_HOST_VISIBILITY=1
+if [[ -z $SHELL_KAWAII_HOST_VISIBILITY ]] ; then
+    # default:1, valid:0 or 1
+    export SHELL_KAWAII_HOST_VISIBILITY=1
 fi
 
-if [ -z "$SHELL_KAWAII_CLI_TO_LOW" ] ; then
-	# default:0, value:0 or 1
-	export SHELL_KAWAII_CLI_TO_LOW=0
+if [[ -z $SHELL_KAWAII_CLI_TO_LOW ]] ; then
+    # default:0, value:0 or 1
+    export SHELL_KAWAII_CLI_TO_LOW=0
 fi
 
-if [ -z "$SHELL_KAWAII_USER_DELIMITER" ] ; then
-	# default:'$', value:string
-	export SHELL_KAWAII_USER_DELIMITER='$'
+if [[ -z $SHELL_KAWAII_USER_DELIMITER ]] ; then
+    # default:'$', value:string
+    export SHELL_KAWAII_USER_DELIMITER='$'
 fi
 
-if [ -z "$SHELL_KAWAII_MORE_PROMPT_COMMAND" ] ; then
-	export SHELL_KAWAII_MORE_PROMPT_COMMAND='echo ""'
+if [[ -z $SHELL_KAWAII_MORE_PROMPT_COMMAND ]] ; then
+    export SHELL_KAWAII_MORE_PROMPT_COMMAND='echo ""'
 fi
 
-if [ -z "$SHELL_KAWAII_AUTO_SET_ZLE_WIDGETS" ] ; then
-	export SHELL_KAWAII_AUTO_SET_ZLE_WIDGETS=1
+if [[ -z $SHELL_KAWAII_AUTO_SET_ZLE_WIDGETS ]] ; then
+    export SHELL_KAWAII_AUTO_SET_ZLE_WIDGETS=1
 fi
--- zsh-shell-kawaii.zsh
+++ zsh-shell-kawaii.zsh
@@ -1,47 +1,47 @@
 #!/usr/bin/env zsh
 
-# Initialize confirations
+# Initialize env vars
 source $(dirname $0)/init.zsh
 
 shell_kawaii_build_prompt () {
-	# Define the variable. It looks like 'john@ArchLinux' or 'john'
-	local user_env="%{$fg[yellow]%}${SHELL_KAWAII_FAKE_USERNAME}%{$fg[green]%}"
-	if [ "$SHELL_KAWAII_HOST_VISIBILITY" -eq 1 ] ; then
-		local user_env="${user_env}@%{$fg[yellow]%}${SHELL_KAWAII_FAKE_HOSTNAME}%{$reset_color%}"
-	fi
+    # Define the variable. It looks like 'john@ArchLinux' or 'john'
+    local user_env="%{$fg[yellow]%}${SHELL_KAWAII_FAKE_USERNAME}%{$fg[green]%}"
+    if [[ $SHELL_KAWAII_HOST_VISIBILITY -eq 1 ]] ; then
+        local user_env="${user_env}@%{$fg[yellow]%}${SHELL_KAWAII_FAKE_HOSTNAME}%{$reset_color%}"
+    fi
 
-	# Define the expression of previous exit code
-	if [ "$SHELL_KAWAII_HER_VISIBILITY" -eq 1 ] ; then
-		# Define the maid. She expresses the state of previous exit code
-		local maid="%(?.%{${fg_bold[green]}%}.%{${fg_bold[blue]}%})%(?!(*^-^)!(;^-^%))%{${reset_color}%}"
-		local feature="${maid}%{$fg[green]%}</%{$fg[yellow]%}${user_env}%{$fg[green]%}/%{$reset_color%}"
-	else
-		local feature="${user_env}"
-	fi
+    # Define the expression of previous exit code
+    if [[ $SHELL_KAWAII_HER_VISIBILITY -eq 1 ]] ; then
+        # Define the maid. She expresses the state of previous exit code
+        local maid="%(?.%{${fg_bold[green]}%}.%{${fg_bold[blue]}%})%(?!(*^-^)!(;^-^%))%{${reset_color}%}"
+        local feature="${maid}%{$fg[green]%}</%{$fg[yellow]%}${user_env}%{$fg[green]%}/%{$reset_color%}"
+    else
+        local feature="${user_env}"
+    fi
 
-	local current_dir="%{$fg[yellow]%}%~%{$reset_color%}"
-	local delim_pattern="%{$fg[cyan]%}%(!.#.${SHELL_KAWAII_USER_DELIMITER})%{$reset_color%}"
+    local current_dir="%{$fg[yellow]%}%~%{$reset_color%}"
+    local delim_pattern="%{$fg[cyan]%}%(!.#.${SHELL_KAWAII_USER_DELIMITER})%{$reset_color%}"
 
-	# Apply result
-	if [ "$SHELL_KAWAII_CLI_TO_LOW" -eq 1 ] ; then
-		#FIXME: How can I set the linebreak without this notation ?
-		export PROMPT="${feature} ${current_dir}%{$reset_color%}$(eval "$SHELL_KAWAII_MORE_PROMPT_COMMAND")
-${delim_pattern} "
-	else
-		export RPROMPT="$(eval "$SHELL_KAWAII_MORE_PROMPT_COMMAND")"
-		export PROMPT="${feature} ${current_dir}${delim_pattern}%{$reset_color%} "
-	fi
+    # Apply result
+    if [[ $SHELL_KAWAII_CLI_TO_LOW -eq 1 ]] ; then
+        #FIXME: How can I set the linebreak without this notation ?
+        export PROMPT="${feature} ${current_dir}%{$reset_color%}$(eval "$SHELL_KAWAII_MORE_PROMPT_COMMAND")
+        ${delim_pattern} "
+    else
+        export RPROMPT="$(eval "$SHELL_KAWAII_MORE_PROMPT_COMMAND")"
+        export PROMPT="${feature} ${current_dir}${delim_pattern}%{$reset_color%} "
+    fi
 }
 
 # Initialize $PROMPT
 shell_kawaii_build_prompt
 
 # Redraw $PROMPT when input was caught or keymap was changed
-if [ "$SHELL_KAWAII_AUTO_SET_ZLE_WIDGETS" -eq 1 ] ; then
-	function zle-line-init zle-keymap-select {
-		shell_kawaii_build_prompt
-		zle reset-prompt
-	}
-	zle -N zle-line-init
-	zle -N zle-keymap-select
+if [[ $SHELL_KAWAII_AUTO_SET_ZLE_WIDGETS -eq 1 ]] ; then
+    function zle-line-init zle-keymap-select {
+        shell_kawaii_build_prompt
+        zle reset-prompt
+    }
+    zle -N zle-line-init
+    zle -N zle-keymap-select
 fi
